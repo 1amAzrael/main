4270dfc8fd0a018a8a3983d3a051fa531ca9eb2b
1amAzrael <sangambkh21@gmail.com>
2024-05-22 21:11:38 +0545

    utils

--- src/index.js
+++ src/index.js
@@ -10,3 +10,11 @@ path: './env'
 
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`Server is running on port ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB CONNECTION FAILED !!!", err);
+})
